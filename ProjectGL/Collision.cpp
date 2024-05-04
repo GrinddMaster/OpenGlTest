7852645b468ae415c35e6ade628da5e7e28ce3b2
GrinddMaster <Ali.Swelam@Student.aast.edu>
2024-05-04 19:21:38 +0300

    Collupdt

---
+++ ProjectGL/Collision.cpp
@@ -0,0 +1,180 @@
+// C program to demonstrate 
+// drawing a circle using 
+// OpenGL 
+//g++ *filename.cpp* -o *FolderName* -lglut -lGLU -lGL
+#include<stdio.h> 
+#include<GL/glut.h> 
+#include<math.h> 
+#include<string.h>
+#include<iostream>
+
+using namespace std;
+#define pi 3.142857 
+
+// function to initializ
+int phyWidth= 700;
+int phyHeight= 700;
+int logWidth=100;
+int logHeight=100;
+int centerX=logWidth/2;
+int centerY=logHeight/2;
+int alphaX,alphaY,flag,countscore,scoreflag = 0;
+
+struct BigSquare{
+    int TopRightCornerX;
+    int TopRightCornerY;
+
+    void TBigSquare()
+{
+    glColor3f(1, 1, 1);
+glBegin(GL_POLYGON);
+glVertex2f(centerX-15/2+alphaX,centerY-15/2+alphaY);
+glVertex2f(centerX+15/2+alphaX,centerY-15/2+alphaY);
+glVertex2f(centerX+15/2+alphaX,centerY+15/2+alphaY);
+glVertex2f(centerX-15/2+alphaX,centerY+15/2+alphaY);
+glEnd();
+TopRightCornerX = centerX-15/2+alphaX;
+TopRightCornerY = centerY-15/2+alphaY;
+}
+   };
+    struct SmallSquare{
+    int TopLeftCornerX,TopRightCornerX;
+    int TopRightCornerY,TopLeftCornerY;
+
+    void TSmallSquare(int r,int g,int b)
+{
+    glColor3f(r, g, b);
+glBegin(GL_POLYGON);
+glVertex2f((centerX+10)-5/2,(centerY+10)-5/2);
+glVertex2f((centerX+10)+5/2,(centerY+10)-5/2);
+glVertex2f((centerX+10)+5/2,(centerY+10)+5/2);
+glVertex2f((centerX+10)-5/2,(centerY+10)+5/2);
+glEnd();
+
+
+//The two points of the frog on the left that will collide
+TopLeftCornerX = (centerX+10)-5/2;
+TopLeftCornerY = (centerY+10)-5/2;
+TopRightCornerX = (centerX+10)+5/2;
+TopRightCornerY = (centerY+10)+5/2;
+}
+    };
+
+    BigSquare Collider;
+    SmallSquare Frog;
+
+void DrawCircle(float cx, float cy, float r, int num_segments) {
+glBegin(GL_POLYGON);
+for (int i = 0; i < num_segments; i++) {
+float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle
+float x = r * cosf(theta);//calculate the x component
+float y = r * sinf(theta);//calculate the y component
+glVertex2f(x + cx, y + cy);//output vertex
+}
+glEnd();
+}
+
+void Collide()
+{
+    if(Collider.TopRightCornerX >= Frog.TopLeftCornerX && Collider.TopRightCornerX <= Frog.TopLeftCornerX
+    && Collider.TopRightCornerY >= Frog.TopLeftCornerY && Collider.TopRightCornerY <= Frog.TopLeftCornerY)
+    {
+        Frog.TSmallSquare(0,0.5,0.5);
+        if(scoreflag == 0)
+        {
+            scoreflag = 1;
+            countscore++;
+        }
+        flag = 1;
+    }
+    if(flag == 0){
+        Frog.TSmallSquare(1,1,1);
+    }
+}
+
+
+
+void printSome(char *str,int x,int y) {
+	
+glColor3f (1, 1.0, 1.0);
+glRasterPos2d(x,y);
+for (int i=0;i<strlen(str);i++){glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,str[i]);}
+glFlush();
+}
+void KeyMoves(unsigned char key, int x, int y) {
+
+    if(key == 'w')
+    {
+        alphaY += 5;
+    }
+    if(key == 's')
+    {
+        alphaY -= 5;
+    }
+    if(key == 'a')
+    {
+        alphaX -= 5;
+    }
+    if(key == 'd')
+    {
+        alphaX += 5;
+    }
+    
+    glutPostRedisplay();
+}
+
+
+
+
+
+
+void init2D (void) 
+{ 
+	// making background color black as first 
+	// 3 arguments all are 0.0 
+	glClearColor(0, 0, 0.5, 0.5); 
+	
+	// making picture color green (in RGB mode), as middle argument is 1.0 
+	glColor3f(0.0, 0.0, 0.5); 
+	
+	// breadth of picture boundary is 1 pixel 
+	glPointSize(2.0); 
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	
+	// setting window dimension in X- and Y- direction 
+	gluOrtho2D(0.0, logWidth, 0.0, logHeight); 
+} 
+
+void Display (void) 
+{ 
+glClear(GL_COLOR_BUFFER_BIT);
+
+string s = to_string(countscore);
+char* str = new char[s.length()+1];
+strcpy(str, s.c_str());
+
+
+Collider.TBigSquare();
+
+
+glutSwapBuffers();
+glFlush();
+
+
+} 
+//g++ Collision.cpp -o Collision -lglut -lGLU -lGL
+
+
+int main (int argc, char** argv) 
+{ 
+	glutInit( &argc, argv);
+glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB);
+glutInitWindowPosition( 650, 100);
+glutInitWindowSize(phyWidth, phyHeight);
+glutCreateWindow( "Assignment4");
+init2D();
+glutDisplayFunc(Display);
+glutKeyboardFunc(KeyMoves);
+glutMainLoop();
+} 
