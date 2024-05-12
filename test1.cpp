ec0d00e770806c616e88eefdeda0f7e77b53f8e0
Ali Hussein Ragab <102867829+GrinddMaster@users.noreply.github.com>
2024-05-12 19:02:14 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,141 +1,279 @@
-// C program to demonstrate 
-// drawing a circle using 
-// OpenGL 
-#include<stdio.h> 
-#include<GL/glut.h> 
-#include<math.h> 
-#include<string.h>
+#include <stdio.h> 
+#include <GL/glut.h> 
+#include <math.h> 
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <ctime> // Include for time functions
+
+using namespace std;
+
 #define pi 3.142857 
 
-// function to initializ
-int phyWidth= 600;
-int phyHeight= 600;
-int logWidth=300;
-int logHeight=300;
-int centerX=logWidth/2;
-int centerY=logHeight/2;
+int phyWidth = 700;
+int phyHeight = 700;
+int logWidth = 100;
+int logHeight = 100;
+int centerX = logWidth / 2;
+int centerY = logHeight / 2;
 
-void DrawCircle(float cx, float cy, float r, int num_segments) {
-glBegin(GL_POLYGON);
-for (int i = 0; i < num_segments; i++) {
-float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle
-float x = r * cosf(theta);//calculate the x component
-float y = r * sinf(theta);//calculate the y component
-glVertex2f(x + cx, y + cy);//output vertex
+struct Road {
+    int W = 12;
+    int H = 12;
+
+    int vx1 = centerX - W / 2, vx2 = centerX + W / 2, vx3 = centerX + W / 2, vx4 = centerX - W / 2; // X vertices
+    int vy1 = centerY - H / 2, vy2 = centerY - H / 2, vy3 = centerY + H / 2, vy4 = centerY + H / 2; // Y vertices
+
+    void CreateRoad(double r, double g, double b) {
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1, vy1); //Bottom left corner
+        glVertex2f(vx2, vy2); //Bottom right corner
+        glVertex2f(vx3, vy3); //Top Right corner
+        glVertex2f(vx4, vy4); //Bottom Right corner
+        glEnd();
+    }
+
+    void ChangeHorizontalPos(int x1, int x2, int x3, int x4, char z) {
+        if (z == '+') {
+            vx1 += x1;
+            vx2 += x2;
+            vx3 += x3;
+            vx4 += x4;
+        }
+        else if (z == '-') {
+            vx1 -= x1;
+            vx2 -= x2;
+            vx3 -= x3;
+            vx4 -= x4;
+        }
+        else {
+            //Do Nothing
+        }
+    }
+
+    void ChangeVerticalPos(int y1, int y2, int y3, int y4, char z) {
+        if (z == '+') {
+            vy1 += y1;
+            vy2 += y2;
+            vy3 += y3;
+            vy4 += y4;
+        }
+        else if (z == '-') {
+            vy1 -= y1;
+            vy2 -= y2;
+            vy3 -= y3;
+            vy4 -= y4;
+        }
+        else {
+            //Do nothing
+        }
+    }
+};
+
+struct RoadStrips {
+    int vx1 = centerX - 5 / 2, vx2 = centerX + 10 / 2, vx3 = centerX + 10 / 2, vx4 = centerX - 5 / 2; // X vertices
+    int vy1 = centerY - 3.2 / 2, vy2 = centerY - 3.2 / 2, vy3 = centerY + 3.2 / 2, vy4 = centerY + 3.2 / 2; // Y vertices
+
+    void BuildRoadStrips(int r, int g, int b) {
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1, vy1); //Bottom Left Corner
+        glVertex2f(vx2, vy2); //Bottom Right corner
+        glVertex2f(vx3, vy3); //Top Right corner
+        glVertex2f(vx4, vy4); //Top left corner
+        glEnd();
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1 + 20, vy1); //Bottom Left Corner
+        glVertex2f(vx2 + 20, vy2); //Bottom Right corner
+        glVertex2f(vx3 + 20, vy3); //Top Right corner
+        glVertex2f(vx4 + 20, vy4); //Top left corner
+        glEnd();
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1 + 40, vy1); //Bottom Left Corner
+        glVertex2f(vx2 + 40, vy2); //Bottom Right corner
+        glVertex2f(vx3 + 40, vy3); //Top Right corner
+        glVertex2f(vx4 + 40, vy4); //Top left corner
+        glEnd();
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1 - 20, vy1); //Bottom Left Corner
+        glVertex2f(vx2 - 20, vy2); //Bottom Right corner
+        glVertex2f(vx3 - 20, vy3); //Top Right corner
+        glVertex2f(vx4 - 20, vy4); //Top left corner
+        glEnd();
+        glColor3f(r, g, b);
+        glBegin(GL_POLYGON);
+        glVertex2f(vx1 - 40, vy1); //Bottom Left Corner
+        glVertex2f(vx2 - 40, vy2); //Bottom Right corner
+        glVertex2f(vx3 - 40, vy3); //Top Right corner
+        glVertex2f(vx4 - 40, vy4); //Top left corner
+        glEnd();
+        glColor3f(r, g, b);
+    }
+
+    void ChangeHorizontalPos(int x1, int x2, int x3, int x4, char z) {
+        if (z == '+') {
+            vx1 += x1;
+            vx2 += x2;
+            vx3 += x3;
+            vx4 += x4;
+        }
+        else if (z == '-') {
+            vx1 -= x1;
+            vx2 -= x2;
+            vx3 -= x3;
+            vx4 -= x4;
+        }
+        else {
+            //Do Nothing
+        }
+    }
+
+    void ChangeVerticalPos(int y1, int y2, int y3, int y4, char z) {
+        if (z == '+') {
+            vy1 += y1;
+            vy2 += y2;
+            vy3 += y3;
+            vy4 += y4;
+        }
+        else if (z == '-') {
+            vy1 -= y1;
+            vy2 -= y2;
+            vy3 -= y3;
+            vy4 -= y4;
+        }
+        else {
+            //Do nothing
+        }
+    }
+};
+
+void printSome(char* str, int x, int y) {
+
+    glColor3f(1, 1.0, 1.0);
+    glRasterPos2d(x, y);
+    for (int i = 0; i < strlen(str); i++) { glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, str[i]); }
+    glFlush();
 }
-glEnd();
+
+void init2D(void) {
+    glClearColor(0, 0.7, 0.0, 0.0); // making background color black as first 3 arguments all are 0.0 
+    glColor3f(0.0, 0.0, 0.5); // making picture color green (in RGB mode), as middle argument is 1.0 
+    glPointSize(2.0); // breadth of picture boundary is 1 pixel 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, logWidth, 0.0, logHeight); // setting window dimension in X- and Y- direction 
 }
 
-void printSome(char *str,int x,int y) {
-	
-glColor3f (1, 1.0, 1.0);
-glRasterPos2d(x,y);
-for (int i=0;i<strlen(str);i++){glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,str[i]);}
-glFlush();
+Road rd1, rd2, rd3;
+RoadStrips rs1, rs2, rs3;
+
+void Road1(int Width, int Height) {
+    rd1.W = Width;
+    rd1.H = Height;
+    rd1.vx1 = centerX - rd1.W / 2;
+    rd1.vx2 = centerX + rd1.W / 2;
+    rd1.vx3 = centerX + rd1.W / 2;
+    rd1.vx4 = centerX - rd1.W / 2;
+    rd1.vy1 = centerY - rd1.H / 2;
+    rd1.vy2 = centerY - rd1.H / 2;
+    rd1.vy3 = centerY + rd1.H / 2;
+    rd1.vy4 = centerY + rd1.H / 2;
 }
 
+void Road2(int Width, int Height) {
+    rd2.W = Width;
+    rd2.H = Height;
+    rd2.vx1 = centerX - rd2.W / 2;
+    rd2.vx2 = centerX + rd2.W / 2;
+    rd2.vx3 = centerX + rd2.W / 2;
+    rd2.vx4 = centerX - rd2.W / 2;
+    rd2.vy1 = centerY - rd2.H / 2;
+    rd2.vy2 = centerY - rd2.H / 2;
+    rd2.vy3 = centerY + rd2.H / 2;
+    rd2.vy4 = centerY + rd2.H / 2;
+}
 
-void init2D (void) 
-{ 
-	// making background color black as first 
-	// 3 arguments all are 0.0 
-	glClearColor(0, 173, 150, 0.4); 
-	
-	// making picture color green (in RGB mode), as middle argument is 1.0 
-	glColor3f(0.0, 1.0, 0.0); 
-	
-	// breadth of picture boundary is 1 pixel 
-	glPointSize(2.0); 
-	glMatrixMode(GL_PROJECTION); 
-	glLoadIdentity(); 
-	
-	// setting window dimension in X- and Y- direction 
-	gluOrtho2D(-780, 780, -420, 420); 
-} 
+void Road3(int Width, int Height) {
+    rd3.W = Width;
+    rd3.H = Height;
+    rd3.vx1 = centerX - rd3.W / 2;
+    rd3.vx2 = centerX + rd3.W / 2;
+    rd3.vx3 = centerX + rd3.W / 2;
+    rd3.vx4 = centerX - rd3.W / 2;
+    rd3.vy1 = centerY - rd3.H / 2;
+    rd3.vy2 = centerY - rd3.H / 2;
+    rd3.vy3 = centerY + rd3.H / 2;
+    rd3.vy4 = centerY + rd3.H / 2;
+}
 
-void Display (void) 
-{ glClear(GL_COLOR_BUFFER_BIT);
-glColor3f(1.0, 1.0, 1.0);
-DrawCircle(-450,300,60,1000);
-DrawCircle(-400,330,60,1000);
-DrawCircle(-350,290,60,1000);	
+// Global variable to store starting time
+time_t startTime;
 
-DrawCircle(450,330,60,1000);
-DrawCircle(400,350,60,1000);
-DrawCircle(350,330,60,1000);	
+// Function to initialize the timer
+void initTimer() {
+    startTime = time(NULL); // Get the current time and store it as the start time
+}
 
+// Function to calculate and display elapsed time
+void displayTimer() {
+    // Calculate elapsed time by subtracting current time from start time
+    time_t currentTime = time(NULL);
+    int elapsedTime = difftime(currentTime, startTime);
 
-glBegin(GL_QUADS);
-glColor3f(0, 0, 0);
-glVertex2i(400,-400);
-glVertex2i(-400,-400);
-glVertex2i(-100,50);
-glVertex2i(100,50);
-glEnd();
+    // Convert elapsed time to minutes and seconds
+    int minutes = elapsedTime / 60;
+    int seconds = elapsedTime % 60;
 
-glBegin(GL_POLYGON);
-glColor3f(1, 1, 0);
-glVertex2i(-800,-400);
-glVertex2i(-350,-400);
-glVertex2i(-100,50);
-glVertex2i(-800,50);
-glEnd();
+    // Convert minutes and seconds to string format
+    char timerString[50];
+    sprintf(timerString, "Time: %02d:%02d", minutes, seconds);
 
-glBegin(GL_POLYGON);
-glColor3f(1, 1, 0);
-glVertex2i(800,-400);
-glVertex2i(350,-400);
-glVertex2i(100,50);
-glVertex2i(800,50);
-glEnd();
+    // Display the timer string on the screen
+    printSome(timerString, 10, 10); // Assuming (10, 10) is the desired position to display the timer
+}
 
-glBegin(GL_QUADS);
-glColor3f(1, 1, 1);
-glVertex2i(25,-400);
-glVertex2i(-25,-400);
-glVertex2i(-25,-300);
-glVertex2i(25,-300);
-glEnd();
+void Display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
 
-glBegin(GL_QUADS);
-glColor3f(1, 1, 1);
-glVertex2i(25,-250);
-glVertex2i(-25,-250);
-glVertex2i(-25,-150);
-glVertex2i(25,-150);
-glEnd();
+    Road1(120, 16);
+    rd1.ChangeVerticalPos(32, 32, 32, 32, '-');
+    rd1.CreateRoad(0, 0, 0);
+    Road2(120, 16);
+    rd2.ChangeVerticalPos(5, 5, 5, 5, '-');
+    rd2.CreateRoad(0, 0, 0);
+    Road3(120, 16);
+    rd3.ChangeVerticalPos(22, 22, 22, 22, '+');
+    rd3.CreateRoad(0, 0, 0);
+    rs1.ChangeVerticalPos(5, 5, 5, 5, '-');
+    rs1.BuildRoadStrips(1, 1, 1);
+    rs2.ChangeVerticalPos(22, 22, 22, 22, '+');
+    rs2.BuildRoadStrips(1, 1, 1);
+    rs3.ChangeVerticalPos(32, 32, 32, 32, '-');
+    rs3.BuildRoadStrips(1, 1, 1);
 
-glBegin(GL_QUADS);
-glColor3f(0.647, 0.165, 0.165);
-glVertex2i(25,-100);
-glVertex2i(-25,-100);
-glVertex2i(-25,150);
-glVertex2i(25,150);
-glEnd();
-glColor3f(1, 1, 1);
-DrawCircle(0,150,90,6);
-glColor3f(1, 0, 0);
-DrawCircle(0,150,85,6);
+    // Display timer
+    displayTimer();
 
-printSome("Stop",-30,150);
+    glutSwapBuffers();
+    glFlush();
+}
 
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowPosition(650, 100);
+    glutInitWindowSize(phyWidth, phyHeight);
+    glutCreateWindow("GameMap");
+    init2D();
 
-glutSwapBuffers();
-glFlush();
+    // Initialize the timer
+    initTimer();
 
-
-} 
-//g++ *filename.cpp* -o *FolderName* -lglut -lGLU -lGL
-
-
-int main (int argc, char** argv) 
-{ 
-	glutInit( &argc, argv);
-glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB);
-glutInitWindowPosition( 300, 300);
-glutInitWindowSize(phyWidth, phyHeight);
-glutCreateWindow( "Assignment1");
-init2D();
-glutDisplayFunc(Display);
-glutMainLoop();
-} 
+    glutDisplayFunc(Display);
+    glutMainLoop();
+    return 0;
+}
